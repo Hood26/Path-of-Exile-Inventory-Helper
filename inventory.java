18d13853687073049dafc8c2160685289316dce2
LordHood <hoodrich10135@hotmail.com>
2019-03-23 03:22:30 -0700

    Inventory Node for items

---
+++ inventory.java
@@ -0,0 +1,16 @@
+public class inventory{
+
+	private class Node{
+		String item;
+		String itemClass;
+		Node next;
+		Node(String item, String itemClass){
+			if(a != null && itemClass != null){
+				this.item = item;
+				this.itemClass = itemClass
+			}
+
+		}
+	}
+
+}
