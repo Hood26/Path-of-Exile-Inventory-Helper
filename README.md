55771aa09e563397ce39fdc7b4c9baba92e73c58
LordHood <hoodrich10135@hotmail.com>
2019-03-23 03:49:32 -0700

    Really bad array checker
    add array checker function

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Path of Exile Inventory Helper
+# 
--- inventory.java
+++ inventory.java
@@ -7,9 +7,41 @@ public class inventory{
 		Node(String item, String itemClass){
 			if(a != null && itemClass != null){
 				this.item = item;
-				this.itemClass = itemClass
+				this.itemClass = itemClass;
+			}
+			else{
+				throw new NullPointerException("cannot create node with null variables");
 			}
 
+		}
+	}
+
+
+	Node inventory;
+	String currentItemClass;
+	String[] classArray = new String[]{'helmet', 'chestplate', 'gloves', 'boots'};
+
+	public inventory(){
+		inventory = null;
+		currentItemClass = "";
+	}
+
+	public static void add(String item, String itemClass){
+		String[] arr = classArray;
+		boolean modifier = false;
+		if(itemClass != null && item != null){
+			int i = 0;
+			if(!(arr[i].equals(itemClass))){
+				i++;
+				while(!(arr[i].equals(itemClass))){
+					i++;
+					if(i == 3){
+						modifier = false;
+						break;
+					}
+
+				}
+			}
 		}
 	}
 
